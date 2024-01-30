f9aef87e87cd491c9dd806488b658f387b154dbb
Chris Pinnock <chris_pinnock@mac.com>
2024-01-30 10:45:52 +0000

    allow drafts

--- hem2jek.sh
+++ hem2jek.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-[ -z "$1" ] && echo "Usage: $0 Hemmingwayfile"
+
+usage() {
+    echo "Usage: $0 [-d] Hemmingwayfile" >&2
+    exit 1
+}
+
+TYPE="post"
+
+while [ $# -gt 0 ]; do
+        case $1 in
+        -d)     TYPE="draft";
+        -*)     usage; ;;
+        *)      break; # rest of args are targets
+        esac
+        shift
+done
+
+[ -z "$1" ] && usage
 file=$1
 
 hmd=@BINDIR@/hem2md
+jekyll="bundle exec jekyll"
 
 if [ ! -x "$hmd" ]; then
     hmd=hem2md
@@ -11,22 +29,22 @@ if [ ! -x "$hmd" ]; then
     [ "$?" != "0" ] && echo "cannot find hem2md in path" && exit 1
 fi
 
+which ansi2txt 2>&1 >/dev/null
+[ "$?" != "0" ] && echo "cannot find ansi2txt in path" && exit 1
+
 tmpfile="$(mktemp /tmp/hemXXXX)"
 $hmd "$file" > "$tmpfile"
 
 # Get the title
 #
-title="$(grep '^#' "$tmpfile" | sed -e 's/^#//g')"
+title="$(grep '^#' "$tmpfile" | sed -e 's/^#[ \t]*//g')"
 
 echo "Title: $title"
 #jekyll post "$title"
-post=$(jekyll post "$title")
-#    [ "$?" != "0" ] && echo "jekyll failed" && exit 1
-
-echo $post
-post=$(echo "$post" | gsed -e 's/\^\[\[.m//g' -e 's/\^\[\[..m//g')
-echo $post
-post=$(echo "$post" | grep '^New post created' | sed -e 's/New post created at //g')
+post=$($jekyll $TYPE "$title")
+[ "$?" != "0" ] && echo "jekyll failed" && exit 1
+post=$(echo "$post" | grep '^New post created' | \
+    sed -e 's/New post created at //g' -e's/[ \t]*$//' | ansi2txt)
 echo "Post:  $post"
 [ ! -f "$post" ] && echo "jekyll failed" && exit 1
 
